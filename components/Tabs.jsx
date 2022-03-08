ac9d232ad7dfdcd38a664815fd5a9671c845209b
Adam Ray <raywibowo68@gmail.com>
2022-03-08 20:44:10 +0700

    Redesign tabs component

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,22 +1,43 @@
 
 export default function Tabs() {
     return (
-        <div className="flex items-center space-x-4 mt-5">
+        <div className="flex items-center space-x-4 mt-5 overflow-auto">
             <div>
-                <button className="text-white font-bold bg-blue-500 py-3 px-6 rounded transition-all focus:bg-blue-700 focus:border-2 focus:border-white">
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
                     Ice Coffee
                 </button>
             </div>
             <div>
-                <button className="text-white font-bold bg-blue-500 py-3 px-6 rounded transition-all focus:bg-blue-700 focus:border-2 focus:border-white">
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
                     Hot Coffee
                 </button>
             </div>
             <div>
-                <button className="text-white font-bold bg-blue-500 py-3 px-6 rounded transition-all focus:bg-blue-700 focus:border-2 focus:border-white">
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
                     Tea
                 </button>
             </div>
+            <div>
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Tea
+                </button>
+            </div>
+            <div>
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Tea
+                </button>
+            </div>
+            <div>
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Tea
+                </button>
+            </div>
+            <div>
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Tea
+                </button>
+            </div>
+
 
 
 
