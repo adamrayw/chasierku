ae4c0da777341a29a0af1ab4961c27aad358661d
Adam Ray <raywibowo68@gmail.com>
2022-03-09 20:22:34 +0700

    Chang bg gray and add name

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -3,41 +3,45 @@ export default function Tabs() {
     return (
         <div className="flex items-center space-x-4 mt-5 overflow-auto">
             <div>
-                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
                     Ice Coffee
                 </button>
             </div>
             <div>
-                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
                     Hot Coffee
                 </button>
             </div>
             <div>
-                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
-                    Tea
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Artisan Tea
                 </button>
             </div>
             <div>
-                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
-                    Tea
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Ice Mojito
                 </button>
             </div>
             <div>
-                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
-                    Tea
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Beverage
                 </button>
             </div>
             <div>
-                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
-                    Tea
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Soft Drink
                 </button>
             </div>
             <div>
-                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-blue-500 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
-                    Tea
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Snacks
                 </button>
             </div>
-
+            <div>
+                <button className="text-gray-600 font-bold bg-white shadow-sm w-40 py-3 px-6 rounded transition-all focus:bg-gray-300 bg-opacity-60 border border-transparent focus:border focus:text-white focus:border-blue-400">
+                    Ice Coffee
+                </button>
+            </div>
 
 
 
