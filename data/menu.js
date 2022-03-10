b463e7501fcb39e287c519f5c4d1661e44e7d566
Adam Ray <raywibowo68@gmail.com>
2022-03-10 20:29:37 +0700

    Dummy data

---
+++ data/menu.js
@@ -0,0 +1,26 @@
+export const menu = [
+  {
+    id: 1,
+    name: "Ice Coffee",
+    image: "/assets/menu/coffee.png",
+    price: "68000",
+  },
+  {
+    id: 2,
+    name: "Ice Coffee",
+    image: "/assets/menu/coffee.png",
+    price: "68000",
+  },
+  {
+    id: 3,
+    name: "Ice Coffee",
+    image: "/assets/menu/coffee.png",
+    price: "68000",
+  },
+  {
+    id: 4,
+    name: "Ice Coffee",
+    image: "/assets/menu/coffee.png",
+    price: "68000",
+  },
+];
