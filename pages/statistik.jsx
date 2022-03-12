ab4059598ae8f19f37ec2113f2897d9cd0ee4619
Adam Ray <raywibowo68@gmail.com>
2022-03-12 19:29:32 +0700

    Add  next/head title

--- pages/statistik.jsx
+++ pages/statistik.jsx
@@ -1,11 +1,15 @@
+import Head from 'next/head'
 import React from 'react'
 import NavbarC from '../components/Navbar'
 
 export default function statistik() {
     return (
-        <>
+        <div>
+            <Head>
+                <title>Statistic</title>
+            </Head>
             <div>statistik</div>
             <NavbarC />
-        </>
+        </div>
     )
 }
