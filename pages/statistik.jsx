6dccbc3504c0248f895deaab8fd2de52fee28162
Adam Ray <raywibowo68@gmail.com>
2022-03-12 19:05:20 +0700

    Statistik Page

---
+++ pages/statistik.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavbarC from '../components/Navbar'
+
+export default function statistik() {
+    return (
+        <>
+            <div>statistik</div>
+            <NavbarC />
+        </>
+    )
+}
