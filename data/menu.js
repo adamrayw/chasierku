31c1222b3482325df0cf9f41d6818830c5d30df8
Adam Ray <raywibowo68@gmail.com>
2022-03-13 20:31:38 +0700

    Update Menu

--- data/menu.js
+++ data/menu.js
@@ -1,26 +1,26 @@
 export const menu = [
   {
     id: 1,
-    name: "Ice Coffee",
+    name: "Hot Coffee",
     image: "/assets/menu/coffee.png",
     price: "68000",
   },
   {
     id: 2,
     name: "Ice Coffee",
-    image: "/assets/menu/coffee.png",
-    price: "68000",
+    image: "/assets/menu/ice_coffee.png",
+    price: "55000",
   },
   {
     id: 3,
-    name: "Ice Coffee",
-    image: "/assets/menu/coffee.png",
-    price: "68000",
+    name: "Soft Drink",
+    image: "/assets/menu/soft_drink.png",
+    price: "8000",
   },
   {
     id: 4,
-    name: "Ice Coffee",
-    image: "/assets/menu/coffee.png",
-    price: "68000",
+    name: "Kentang Goreng",
+    image: "/assets/menu/kentang_goreng.png",
+    price: "26000",
   },
 ];
