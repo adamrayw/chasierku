9c40edda318eec1a88384f688170b016347099ef
Adam Ray <raywibowo68@gmail.com>
2022-03-14 22:20:01 +0700

    Wrap component with Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import "@material-tailwind/react/tailwind.css";
+import { Provider } from "react-redux";
+import { store } from "../app/store";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
