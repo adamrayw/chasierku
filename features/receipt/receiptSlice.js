9c362759d78eb951dcd8209342c54d09707b7b2a
Adam Ray <raywibowo68@gmail.com>
2022-03-17 21:29:15 +0700

    add removeReceiptItem reducer

--- features/receipt/receiptSlice.js
+++ features/receipt/receiptSlice.js
@@ -12,9 +12,20 @@ export const receiptSlice = createSlice({
       const tempMenu = { ...action.payload };
       state.value.push(tempMenu);
     },
+    removeReceiptItem: (state, action) => {
+      // get id from state value
+      const itemIndex = state.value.findIndex(
+        (index) => index.id === action.payload.id
+      );
+      if (itemIndex >= 0) {
+        if (state.value[itemIndex]) {
+          state.value.splice(itemIndex, 1);
+        }
+      }
+    },
   },
 });
 
-export const { addToReceipt } = receiptSlice.actions;
+export const { addToReceipt, removeReceiptItem } = receiptSlice.actions;
 
 export default receiptSlice.reducer;
