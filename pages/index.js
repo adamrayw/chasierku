0c78024baac473cd27dffd51fb49529c9015a7e9
Adam Ray <raywibowo68@gmail.com>
2022-03-27 22:51:25 +0700

    Digital clock added

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,33 @@ import NavbarC from "../components/Navbar";
 import Receipt from "../components/Receipt";
 import Search from "../components/Search";
 import Tabs from "../components/Tabs";
-import { useCookies, getAll } from "react-cookie";
-
-import { useEffect } from "react";
+import { useCookies } from "react-cookie";
+import { useEffect, useState } from "react";
 import { Router, useRouter } from "next/router";
 
 export default function Home() {
   const router = useRouter();
   const [cookie, setCookie] = useCookies(["user"]);
+  const [clock, setClock] = useState();
 
   useEffect(() => {
     if (!cookie.user) {
-      router.push("/auth/login")              ;
+      router.push("/auth/login");
     } else {
       return;
     }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  setInterval(() => {
+    const date = new Date();
+    setClock(
+      date.toLocaleTimeString(["ban", "id"], {
+        hour: "2-digit",
+        minute: "2-digit",
+      })
+    );
+  }, 1000);
 
   return (
     <div>
@@ -33,7 +42,10 @@ export default function Home() {
       <main>
         <section className="grid grid-cols-3 ">
           <div className="col-span-2 bg-gray-50 p-10 h-screen">
-            <Search />
+            <div className="flex items-center justify-between">
+              <Search />
+              <h1 className="text-gray-500">{clock}</h1>
+            </div>
             <Tabs />
             <Menu />
           </div>
