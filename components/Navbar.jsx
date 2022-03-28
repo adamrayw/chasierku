99b7f6b6bf2e073cf97d2873abc87bb89032ae28
Adam Ray <raywibowo68@gmail.com>
2022-03-29 06:42:01 +0700

    add index 2 which is Modal for Tambah Voucher

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -84,7 +84,7 @@ export default function NavbarC() {
                                             </button>
                                         </li>
                                         <li>
-                                            <button className='flex items-center w-full shadow px-3 py-4 rounded-xl hover:shadow-lg transition-all'>
+                                            <button className='flex items-center w-full shadow px-3 py-4 rounded-xl hover:shadow-lg transition-all' onClick={() => setIndex(2)}>
                                                 <Image src={Discount} width={48} height={48} alt='add-discount' />
                                                 <p className='ml-2'>
                                                     Tambah Voucher Diskon
@@ -137,7 +137,39 @@ export default function NavbarC() {
                                 </div>
                             </div>
                         </div >
-                    ) : index === 2 ? console.log(2) : null
+                    ) : index === 2 ? (
+                        <div className={`modal fixed w-full h-full top-0 left-0 flex items-center justify-center`} >
+                            <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50" onClick={() => setActive(false)}></div>
+
+                            <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-10 overflow-y-auto">
+
+                                <div className="modal-content py-4 text-left px-6">
+                                    <div className="flex justify-between items-center pb-3">
+                                        <div>
+                                            <p onClick={() => setIndex(0)}>back</p>
+                                            <p className="text-2xl font-bold text-gray-600">Tambah Voucher</p>
+                                        </div>
+                                        <div className="modal-close cursor-pointer z-50" onClick={() => setActive(false)}>
+                                            <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                                            </svg>
+                                        </div>
+                                    </div>
+                                    <hr />
+                                    <ul className='space-y-4 my-6'>
+                                        <li>
+                                            <button className='flex items-center w-full shadow px-3 py-4 rounded-xl hover:shadow-lg transition-all'>
+                                                <Image src={Discount} width={48} height={48} alt='add-discount' />
+                                                <p className='ml-2'>
+                                                    Tambah Voucher Diskon
+                                                </p>
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div >
+                    ) : null
                 ) : null
             }
 
