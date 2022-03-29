e866f066b0ffb8b5268a4251c85cd34c4869abf4
Adam Ray <raywibowo68@gmail.com>
2022-03-29 13:02:09 +0700

    Improve addvoucher form

--- components/Modal/Voucher.jsx
+++ components/Modal/Voucher.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Spinner from '../../public/assets/spinner.png';
+import Image from 'next/image';
 import { useCookies } from 'react-cookie';
 
 export default function Voucher() {
@@ -12,6 +14,7 @@ export default function Voucher() {
         disc: '',
     })
     const [loading, setLoading] = useState(false);
+    const [success, setSuccess] = useState(false);
 
     const data = {
         user_id: cookie.user.data.id,
@@ -31,6 +34,11 @@ export default function Voucher() {
             const result = response;
             console.log(result);
             setLoading(false);
+            setSuccess(true);
+            setTimeout(() => {
+                setSuccess(false);
+            }, 3000);
+            setVoucher({ ...voucher, voucher_name: '', voucher_code: '', disc: '' })
         } catch (error) {
             console.log(error);
             setLoading(false);
@@ -40,30 +48,39 @@ export default function Voucher() {
 
 
     return (
-        <div>
-            <div className="block py-6 bg-white max-w-sm">
+        <div className='py-6'>
+            <div className='mb-6'>
+                <h1 className='text-gray-700'>Voucher Aktif</h1>
+            </div>
+            <div className="block bg-white max-w-sm">
                 <form onSubmit={handleSubmit}>
                     <div className="form-group mb-6">
                         <label htmlFor="nama_voucher" className="form-label inline-block mb-2 text-gray-700">Nama Voucher</label>
                         <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
-                            aria-describedby="voucher" placeholder="Nama Voucher" onChange={(e) => setVoucher({ ...voucher, voucher_name: e.target.value })} />
+                            aria-describedby="voucher" placeholder="Nama Voucher" onChange={(e) => setVoucher({ ...voucher, voucher_name: e.target.value })} value={voucher.voucher_name} required />
                         <small id="voucher" className="block mt-1 text-xs text-gray-600">cth: VOUCHER HARI KEMERDEKAAN</small>
                     </div>
                     <div className="form-group mb-6">
                         <label htmlFor="voucher_code" className="form-label inline-block mb-2 text-gray-700">Voucher Code</label>
                         <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
-                            placeholder="Code" onChange={(e) => setVoucher({ ...voucher, voucher_code: e.target.value })} />
+                            placeholder="Code" onChange={(e) => setVoucher({ ...voucher, voucher_code: e.target.value })} value={voucher.voucher_code} />
                         <small id="voucherCode" className="block mt-1 text-xs text-gray-600">cth: MERDEKA45</small>
                     </div>
                     <div className="form-group mb-6">
                         <label htmlFor="jumlah_diskon" className="form-label inline-block mb-2 text-gray-700">Jumlah Diskon</label>
                         <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
-                            placeholder="Jumlah Diskon" onChange={(e) => setVoucher({ ...voucher, disc: e.target.value })} />
+                            placeholder="Jumlah Diskon" onChange={(e) => setVoucher({ ...voucher, disc: e.target.value })} value={voucher.disc} required />
                         <small id="jumlah_diskon" className="block mt-1 text-xs text-gray-600">cth: 40 = 40%</small>
                     </div>
 
-                    <button type="submit" className="px-6 py-2.5 bg-orange-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-700 hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out" >Tambah</button>
-                    {loading ? 'Loading...' : ''}
+                    <button type="submit" className={`px-6 py-2.5 bg-orange-600 ${loading ? 'opacity-50 cursor-not-allowed hover:bg-orange-600 shadow-lg' : ''} text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out `} {...(loading) ? { disabled: true } : {}}>
+                        {loading ? (
+                            <Image className='animate-spin' src={Spinner} width="20" height="20" alt='loading' />
+                        ) : 'TAMBAH'}
+                    </button>
+                    {success ? (
+                        <p className='text-center mt-10 text-green-500'>Voucher Berhasil Ditambahkan!</p>
+                    ) : ''}
                 </form>
             </div>
         </div>
