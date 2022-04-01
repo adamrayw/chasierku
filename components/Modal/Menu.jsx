b4449d9f4381749819df73a2d78ee6db477aaa93
Adam Ray <raywibowo68@gmail.com>
2022-04-01 19:37:04 +0700

    Add cloudinary hostname, and get menu

--- components/Modal/Menu.jsx
+++ components/Modal/Menu.jsx
@@ -19,7 +19,7 @@ export default function Menu() {
     const getMenus = async () => {
         try {
             const response = await axios.get('/api/menu/' + cookie.user.data.id);
-            console.log(response);
+            setMenu(response.data.data.menus);
         } catch (error) {
             console.log(error);
         }
@@ -27,19 +27,19 @@ export default function Menu() {
 
     const handleSubmit = () => { }
     return (
-        <div className="block mt-6 bg-white max-w-full">
+        <div className="block pb-6 bg-white max-w-full">
             <div className='py-6'>
                 <table>
                     <thead>
                         <tr>
                             <th className='sticky w-full top-0 px-6 py-2 text-white bg-orange-500'>
-                                Image
+                                Gambar
                             </th>
                             <th className='sticky w-full top-0 px-6 py-2 text-white bg-orange-500'>
                                 Name
                             </th>
                             <th className='sticky w-full top-0 px-6 py-2 text-white bg-orange-500'>
-                                Price
+                                Harga
                             </th>
                             <th className='sticky w-full z-50 top-0 px-6 py-2 text-white bg-orange-500'>
                                 Action
@@ -47,52 +47,66 @@ export default function Menu() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className='px-6 py-4'>
-                            </td>
-                        </tr>
+                        {menu.map((item) => {
+                            return (
+                                <tr key={item.id}>
+                                    <td className='px-6 py-4'>
+                                        <Image loader={() => item.image} src={item.image} width={60} height={60} unoptimized alt="logo" />
+                                    </td>
+                                    <td className='px-6 py-4'>
+                                        {item.name}
+                                    </td>
+                                    <td className='px-6 py-4'>
+                                        {item.price}
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </div>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group mb-6">
-                    <label htmlFor="nama_voucher" className="form-label inline-block mb-2 text-gray-700">Nama Menu</label>
-                    <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
-                        aria-describedby="voucher" placeholder="Nama Menu" required />
-                    <small id="voucher" className="block mt-1 text-xs text-gray-600">contoh: Coca - cola</small>
-                </div>
-                <div className="form-group mb-6">
-                    <label htmlFor="voucher_code" className="form-label inline-block mb-2 text-gray-700">Gambar</label>
-                    <input type="file" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
-                    />
-                </div>
-                <div className="form-group mb-6">
-                    <label htmlFor="jumlah_diskon" className="form-label inline-block mb-2 text-gray-700">Harga</label>
-                    <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
-                        placeholder="Harga Menu" required />
-                    <small id="jumlah_diskon" className="block mt-1 text-xs text-gray-600">contoh: 40000 = Rp40,000</small>
-                </div>
+            <hr />
+            <div className="block mt-6 bg-white max-w-full">
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group mb-6">
+                        <label htmlFor="nama_voucher" className="form-label inline-block mb-2 text-gray-700">Nama Menu</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
+                            aria-describedby="voucher" placeholder="Nama Menu" required />
+                        <small id="voucher" className="block mt-1 text-xs text-gray-600">contoh: Coca - cola</small>
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="voucher_code" className="form-label inline-block mb-2 text-gray-700">Gambar</label>
+                        <input type="file" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
+                        />
+                    </div>
+                    <div className="form-group mb-6">
+                        <label htmlFor="jumlah_diskon" className="form-label inline-block mb-2 text-gray-700">Harga</label>
+                        <input type="text" className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
+                            placeholder="Harga Menu" required />
+                        <small id="jumlah_diskon" className="block mt-1 text-xs text-gray-600">contoh: 40000 = Rp40,000</small>
+                    </div>
 
-                {isEditMode ? (
-                    <button type="submit" className={`px-6 py-2.5 bg-orange-600 ${loading ? 'opacity-50 cursor-not-allowed hover:bg-orange-600 shadow-lg' : ''} text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out `} {...(loading) ? { disabled: true } : {}} onClick={(() => {
-                        handleEditVoucher();
-                    })}>
-                        {loading ? (
-                            <Image className='animate-spin' src={Spinner} width="20" height="20" alt='loading' />
-                        ) : 'EDIT'}
-                    </button>
-                ) : (
-                    <button type="submit" className={`px-6 py-2.5 bg-orange-600 ${loading ? 'opacity-50 cursor-not-allowed hover:bg-orange-600 shadow-lg' : ''} text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out `} {...(loading) ? { disabled: true } : {}}>
-                        {loading ? (
-                            <Image className='animate-spin' src={Spinner} width="20" height="20" alt='loading' />
-                        ) : 'TAMBAH'}
-                    </button>
-                )}
+                    {isEditMode ? (
+                        <button type="submit" className={`px-6 py-2.5 bg-orange-600 ${loading ? 'opacity-50 cursor-not-allowed hover:bg-orange-600 shadow-lg' : ''} text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out `} {...(loading) ? { disabled: true } : {}} onClick={(() => {
+                            handleEditVoucher();
+                        })}>
+                            {loading ? (
+                                <Image className='animate-spin' src={Spinner} width="20" height="20" alt='loading' />
+                            ) : 'EDIT'}
+                        </button>
+                    ) : (
+                        <button type="submit" className={`px-6 py-2.5 bg-orange-600 ${loading ? 'opacity-50 cursor-not-allowed hover:bg-orange-600 shadow-lg' : ''} text-white font-medium text-xs leading-tight uppercase rounded shadow-md  hover:shadow-lg focus:bg-orange-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out `} {...(loading) ? { disabled: true } : {}}>
+                            {loading ? (
+                                <Image className='animate-spin' src={Spinner} width="20" height="20" alt='loading' />
+                            ) : 'TAMBAH'}
+                        </button>
+                    )}
 
-                {success ? (
-                    <p className='text-center mt-10 text-green-500'>Voucher Berhasil Ditambahkan!</p>
-                ) : ''}
-            </form>
+                    {success ? (
+                        <p className='text-center mt-10 text-green-500'>Voucher Berhasil Ditambahkan!</p>
+                    ) : ''}
+                </form>
+            </div>
         </div>
     )
 }
