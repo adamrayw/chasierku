ae65aac9158a7038447401b056a0f645e582fe9c
Adam Ray <raywibowo68@gmail.com>
2022-04-27 21:21:14 +0700

    change border color when focus

--- pages/auth/login/index.jsx
+++ pages/auth/login/index.jsx
@@ -84,7 +84,7 @@ export default function Login() {
                                 <div className="mb-6">
                                     <input
                                         type="email"
-                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
                                         id="exampleFormControlInput2"
                                         placeholder="Email address"
                                         required
@@ -96,7 +96,7 @@ export default function Login() {
                                 <div className="mb-6">
                                     <input
                                         type="password"
-                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-orange-600 focus:outline-none"
                                         id="exampleFormControlInput2"
                                         placeholder="Password"
                                         required
@@ -108,7 +108,7 @@ export default function Login() {
                                     <div className="form-group form-check">
                                         <input
                                             type="checkbox"
-                                            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                                            className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-orange-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
                                             id="exampleCheck2"
                                         />
                                         <label className="form-check-label inline-block text-gray-800" htmlFor="exampleCheck2"
