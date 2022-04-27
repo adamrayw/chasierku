1f9bb3c2231cf547573d8acdaf82bc428b8b782e
Adam Ray <52489625+adamrayw@users.noreply.github.com>
2022-04-27 21:33:32 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -69,7 +69,7 @@ export default function Home() {
                   </svg>
                 </button>
                 {dropdown ? (
-                  <div class="absolute right-0 bg-white py-2 px-6 shadow">
+                  <div className="absolute right-0 bg-white py-2 px-6 shadow">
                     <button className="flex items-center" onClick={handleLogOut}>
                       <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 pr-1 text-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
